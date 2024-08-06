88e5747e072fd432fb0b65e5695faf2b130db800
Mashopy <eliasgheeraert@gmail.com>
2024-08-06 20:56:05 +0200

    sepolicy: Introduce initial rules for MediaTek devices
    Change-Id: If26e22a0c70590fbf125ddf8883de7bdcb55a4fd

---
+++ sepolicy/mtk/SEPolicy.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    hardware/oplus/sepolicy/mtk/vendor
+
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+    hardware/oplus/sepolicy/mtk/private
+
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
+    hardware/oplus/sepolicy/mtk/public
